6feb254fc3cd2d4b9eaa6ff72bd1018b0f93f606
mj9246N <mjfull603@gmail.com>
2026-08-01 22:23:07 +0330

    Update Doctor.kt

--- app/src/main/java/com/example/doctorschedule/Doctor.kt
+++ app/src/main/java/com/example/doctorschedule/Doctor.kt
@@ -3,6 +3,5 @@ package com.example.doctorschedule
 data class Doctor(
     val name: String,
     val specialty: String,
-    val day: String,
-    val time: String
+    val schedules: List<String>   // هر عضو مثل «چهارشنبه 14 مرداد (08:00-09:30)»
 )
